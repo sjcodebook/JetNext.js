e4175aef11fd465751d5729a649ffd7463c48c98
0xSJ <jainsanmati846@gmail.com>
2025-05-20 14:57:23 +0530

    Add environment configuration with @t3-oss/env-nextjs and zod validation; update package dependencies

---
+++ src/env.ts
@@ -0,0 +1,26 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().min(1),
+    NODE_ENV: z.string().optional(),
+    AUTH_SECRET: z.string().min(1),
+    HOST_NAME: z.string().min(1),
+    HASH_SALT: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
+    AUTH_TRUST_HOST: z.string().min(1),
+  },
+  client: {},
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    HOST_NAME: process.env.HOST_NAME,
+    HASH_SALT: process.env.HASH_SALT,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+  },
+})
